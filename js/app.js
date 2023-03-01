50c3c1f36e1339dff3f72c677c95089f3ecc2337
sofiulaloe <sahasofiulalom3@gmail.com>
2023-03-01 19:50:29 +0600

    news-detelis  api lowd add commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-const loawdNewscatagory = async() =>{
-  const url=` https://openapi.programming-hero.com/api/news/categories`;
-   const res= await fetch(url);
+  
+  const loawdNewscatagory = async() =>{
+   const res= await fetch(`https://openapi.programming-hero.com/api/news/categories`);
+   
    const data= await res.json();
    displaylowadManu(data.data)
+  
 }
 
 const displaylowadManu= (categories) =>{
@@ -10,7 +12,7 @@ const displaylowadManu= (categories) =>{
    const newsContainer=document.getElementById('news-container')
   
   for(const newscatagoris   in categories){
-    
+   
    const newsdiv=document.createElement('li')
    newsdiv.classList.add('liststyle')     
    newsdiv.innerHTML= `
@@ -27,6 +29,43 @@ const displaylowadManu= (categories) =>{
    newsContainer.appendChild(newsdiv)
  }
 }
+
+const loawdNewsdetailes= async()=>{
+const res = await fetch(`https://openapi.programming-hero.com/api/news/category/08`)
+   const data =await res.json();
+   displayNewsLoawd(data.data);
+
+}
+const displayNewsLoawd=(newsDetails)=>{
+   console.log(newsDetails);
+  const userTempletnews=document.getElementById('user-templetnews');
+  newsDetails.forEach(news => {
+    const templetnewsDiv=document.createElement('div');
+templetnewsDiv.classList.add('col')
+templetnewsDiv.innerHTML=`
+      <div class="card  p-3 h-100">
+        <img src="${news.thumbnail_url}" class="card-img-top img-fluid " style="width:100%" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${news.title}</h5>
+         <p class="card-text"> ${news.details} </p>
+         <div class="d-flex"> 
+         <div class="w-50 d-flex">
+         <img src="${news.author.img}" class="card-img-end img-fluid rounded-circle" style="width:20%;">
+         <p class="ms-2"> ${news.author.name} </P>
+         </div>
+         <div class="text-center"> 
+         
+         <p> view ${news.total_view} </p>
+         </div>
+          </div>
+        </div>
+      </div>
+`;
+   userTempletnews.appendChild(templetnewsDiv)
+  });
+}
+
+loawdNewsdetailes()
 
  
 loawdNewscatagory()
